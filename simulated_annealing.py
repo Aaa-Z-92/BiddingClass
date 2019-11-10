ab2e864a44209df1f296352c990cc1edebff99a4
A.Z <amandazhangpp@gmail.com>
2019-11-10 10:44:30 -0800

    Complete combination validation

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -6,9 +6,22 @@ def get_config():
     config.read('config.ini')
     return config
 
+def time_overlap(class_info, other_class_info):
+    if class_info['Times'][0] > other_class_info['Times'][1]:
+        class_info, other_class_info = other_class_info, class_info
+    if class_info['Times'][1] > other_class_info['Times'][0]:
+        return class_info['Sem'] ^ other_class_info['Sem'] < 3
+    return False
+
 def valid(config, class_infos):
     if (class_infos['Units'].sum() < config.getfloat('BASIC', 'RequiredCredits')):
         return False
+    for class_info in class_infos:
+        for other_class_info in class_infos:
+            if class_info['Course'] == other_class_info['Course']:
+                continue
+            if time_overlap(class_info, other_class_info):
+                return False
     return True
 
 if __name__ == '__main__':
